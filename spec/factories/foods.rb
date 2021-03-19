85e3e657721cd1e40a269b4646345f027cd7532c
tnakamura0314 <reggae.a0215@gmail.com>
2021-03-19 23:38:48 +0900

    レスポンシブデザインの実装

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :food do
     name {Faker::Food.vegetables} 
     number {Faker::Number.between(from: 300, to: 999_999_999)}
-    quantity_id {Faker::Number.between(from: 2, to: 17)}
+    quantity_id {Faker::Number.between(from: 2, to: 18)}
     deadline {"2021-01-01"}
     status {0}
     association :user

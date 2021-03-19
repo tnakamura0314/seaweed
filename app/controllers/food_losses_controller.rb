70fc30350e02dc577ee4f89edb9ae04f45f5c8ae
tnakamura0314 <reggae.a0215@gmail.com>
2021-03-20 00:34:23 +0900

    一覧ページの並べ替え2

--- app/controllers/food_losses_controller.rb
+++ app/controllers/food_losses_controller.rb
@@ -2,6 +2,6 @@ class FoodLossesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @foods = Food.order("deadline ASC")
+    @foods = Food.order("deadline DESC")
   end
 end

442d0040a4965e16cc371dafbc1fba5d6b99ee6a
tnakamura0314 <reggae.a0215@gmail.com>
2021-03-24 16:28:31 +0900

    postのテストコードの実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+    text {Faker::Food.fruits}
+    checked {Faker::Number.between(from: 0, to: 1)}
+    association :user
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.build(:post, user_id: @user.id)
+  end
+
+  describe '登録機能' do
+
+    context '登録できるとき' do
+      it 'すべての値が正しく入力されていれば登録できる' do
+        expect(@post).to be_valid
+      end
+      it 'textが空でも登録できる' do
+        @post.text = ''
+        expect(@post).to be_valid
+      end
+      it 'checkedが空でも登録できる' do
+        @post.checked = ''
+        expect(@post).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it 'user_idが空だと登録できない' do
+        @post.user_id = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("User can't be blank")
+      end
+    end
+  end
 end

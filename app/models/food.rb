3d9261956492e69d654cc3c9ffd6f6220b996c0f
tnakamura0314 <reggae.a0215@gmail.com>
2021-04-12 11:49:12 +0900

    foodのエラー文の日本語仮

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,12 +2,12 @@ class Food < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :number, format: {with:  /\A[0-9]+\z/, message: "half-width characters"}
+    validates :number, format: {with:  /\A[0-9]+\z/, message: "は半角数字で入力してください"}
     validates :deadline
     validates :user
   end
 
-  validates :quantity_id,numericality: { other_than: 1, message: "Select"}
+  validates :quantity_id,numericality: { other_than: 1, message: "を選択してください"}
 
   belongs_to :user
   has_one_attached :image

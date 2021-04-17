ad3f72033da53af3f1275c64bee486039bc58c32
tnakamura0314 <reggae.a0215@gmail.com>
2021-04-17 19:29:39 +0900

    recipeコントローラーのnewとcreateの記述

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,21 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:image, :name, :food_text, :rcipe_text).merge(user_id: current_user.id)
+  end
 end
